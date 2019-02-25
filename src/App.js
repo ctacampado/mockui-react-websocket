81e67fc651a13d049581e1c489e388848e2c2b6e
Carl Acampado <ctacampado@gmail.com>
2019-02-26 04:39:43 +0800

    stuff

--- src/App.js
+++ src/App.js
@@ -3,32 +3,73 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    // this is an "echo" websocket service
-  	var connection = new WebSocket('ws://localhost:8080/ws');
-    // listen to onmessage event
-    ///this.connection.onmessage = evt => { 
-      // add the new message to state
-    	//this.setState({
-      	//messages : this.state.messages.concat([ evt.data ])
-    connection.onopen = function () {
-        var emp = {
-            Firstname:"Bat",
-            Lastname:"Man",
-            TaxID:"1QAZ-2WSX-3EDC",
-            Employer:"XYC Inc.",
-            FPCode:"fpa",
-            Scheme:"Scheme 1"
-        }
-        var msg = {
-            Clienttype:"employer",
-            Action:"enrollment_request",
-            Data:JSON.stringify(emp)
-       }
-       console.log(msg)
-       connection.send(JSON.stringify(msg))
+  constructor() {
+    super();
+    this.payload = {};
+    this.socket = new WebSocket('ws://localhost:8080/ra');
+    this.emit = this.emit.bind(this);
+  }
+
+  emit() {
+    if(this.payload.Payload.EnrollmentApproval) {
+      console.log("received enrollmentApproval!");
+      var msg = {
+        Username:"approver",
+        Type:"RA",
+        OrgCode: "RA"
+      }
+      this.payload.Payload.EnrollmentApproval.Approvals.RAApproval.Status = "approved";
+      this.payload.Payload.EnrollmentApproval.Approvals.RAApproval.Date = new Date();
+      msg.Payload = this.payload.Payload;
+      console.log("sending: " + JSON.stringify(msg));
+      this.socket.send(JSON.stringify(msg));
     }
   }
+
+  componentDidMount() {
+    this.socket.onopen = () => {
+      var empData = {
+        ID:         "52RFV5TGB6YHN",
+        FirstName:  "Yujin",
+        MiddleName: "",
+        LastName:   "Ahn",
+        Birthday:   "01/09/2003",
+        Age:        "15",
+        Address:    "577-92 Banpo sa-dong, Seocho-gu, Seoul, South Korea",
+        Email:      "yujin.ahn@starshipent.kr",
+        ContactNo:  "",
+        TaxID:      "8765432109",
+        Employer:   "Starship Entertainment",
+      }
+      var fpinfo = {
+        FPName: "Fund Provider A",
+        FPCode: "FPA",
+        FPProgram: "A",
+        CurrInvstAmt: "300000.00",
+        Currency: "USD"
+      }
+      var payload = {
+        EnrollmentReq: {
+          EmployeeData: empData,
+          FPInfo: fpinfo
+        }
+      }
+      var msg = {
+        Username:"employee",
+        Type:"RA",
+        OrgCode: "RA",
+        Payload: payload
+      }
+      console.log(msg)
+      this.socket.send(JSON.stringify(msg))
+    }
+    
+    this.socket.onmessage = (event) => {
+      console.log(event.data);
+      this.payload = JSON.parse(event.data);
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -46,6 +87,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <button onClick={this.emit}/>
       </div>
     );
   }
